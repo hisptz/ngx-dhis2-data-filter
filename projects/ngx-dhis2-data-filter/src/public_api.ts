1d4d86cbf3f98bd979d48113026bc6a66fb386a1
Rajab Mkomwa <rajeyw@gmail.com>
2019-06-09 18:17:52 +0300

    refactor: expose models to public

--- projects/ngx-dhis2-data-filter/src/public_api.ts
+++ projects/ngx-dhis2-data-filter/src/public_api.ts
@@ -3,3 +3,13 @@
  */
 
 export * from './lib/ngx-dhis2-data-filter.module';
+export * from './lib/models/function.model';
+export * from './lib/models/function-rule.model';
+export * from './lib/models/indicator.model';
+export * from './lib/models/indicator-group.model';
+export * from './lib/models/data-element.model';
+export * from './lib/models/data-element-group.model';
+export * from './lib/models/data-filter-preference.model';
+export * from './lib/models/data-filter-selection.model';
+export * from './lib/models/data-filter.model';
+export * from './lib/models/data-group.model';
